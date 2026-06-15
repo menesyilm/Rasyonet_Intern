213ad0a52bdb938b75a5e38cee9c43d0acf3bc6a
Muhammed Enes Yılmaz <menes.yilm@gmail.com>
2026-06-15 13:38:24 +0300

    feat: add initial setup for React application with Vite
    - Added SVG assets for React and Vite logos.
    - Created global CSS variables and styles in index.css.
    - Implemented main entry point in main.jsx for rendering the App component.
    - Configured Vite with React plugin in vite.config.js.

---
+++ Rasyonet_Intern.API/Controllers/FonsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rasyonet_Intern.API.Data;
+using Rasyonet_Intern.API.DTOs;
+
+namespace Rasyonet_Intern.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FonsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public FonsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> FonList()
+        {
+            var fonList = await _context.fonPerformanslar.
+                Include(x => x.FonKategori)
+                .Select(x => new FonPerformansDto
+                {
+                    FonPerformansId = x.FonPerformansId,
+                    FonKodu = x.FonKodu,
+                    FonAdi = x.FonAdi,
+                    Buyukluk = x.Buyukluk,
+                    Fiyat = x.Fiyat,
+                    GunlukDegisimYuzde = x.GunlukDegisimYuzde,
+                    HaftalikDegisimYuzde = x.HaftalikDegisimYuzde,
+                    AylikDegisimYuzde = x.AylikDegisimYuzde,
+                    KategoriAdi = x.FonKategori.KategoriAdi
+                })
+                .ToListAsync();
+            return Ok(fonList);
+        }
+    }
+}
